349a2fd736e9e154fb39e1229f0f6174d30a8de4
Harish Thuwal <hthuwal@gmail.com>
2019-02-08 22:37:08 +0530

    Consolidate the Predictions
    Into a single csv file where each row is
    
    image_name,x1,x2,y1,y2

---
+++ yolov3/consolidate-predictions.py
@@ -0,0 +1,21 @@
+import csv
+import os
+from tqdm import tqdm
+
+pred_dir = "hc/predictions"
+
+with open("hc/test.csv") as f, open("hc/prediction_csvs/predictions.csv", "w") as out:
+    reader = csv.reader(f)
+    next(reader)
+    for row in tqdm(reader, total=12815):
+        image = row[0]
+        pred_file = os.path.join(pred_dir, image + ".txt")
+        if os.path.exists(pred_file):
+            with open(pred_file) as pred:
+                values = pred.read().strip().split()
+        else:
+            values = ["0"] * 4
+
+        values.insert(0, image)
+        print(values)
+        out.write(",".join(values) + "\n")
