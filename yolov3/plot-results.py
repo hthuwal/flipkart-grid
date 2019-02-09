c53cf76dd1b478ea2868df0b8bf89cd3b74cc538
Harish Thuwal <hthuwal@gmail.com>
2019-02-09 16:31:43 +0530

    Plot the training metrics

---
+++ yolov3/plot-results.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import sys
+
+titles = ["xloss", "yloss", "wloss", "hloss", "cnfloss", "clsloss", "lossloss", "maPvalid", "Pvalid", "Rvalid"]
+
+results_file = sys.argv[1]
+results = np.loadtxt(results_file, dtype=str)
+results = np.delete(results, [1, 9, 10, 11, 12, 13, 14, 15], axis=1).astype(float)
+epochs = results[:, 0]
+results = results[:, 1:]
+
+plt.clf()
+my_dpi = 300
+plt.figure(figsize=(1920 / my_dpi, (1000 * 5) / my_dpi), dpi=my_dpi)
+colors = [cm.jet(x) for x in np.linspace(0.00, 1.00, len(titles))]
+
+for i in range(len(titles)):
+    plt.subplot(5, 2, i + 1)
+    plt.plot(epochs, results[:, i], color=colors[i], label=titles[i])
+    plt.legend()
+
+plt.savefig("plots/test.png", dpi=my_dpi)
